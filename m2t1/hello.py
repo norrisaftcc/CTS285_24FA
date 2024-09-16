a8d63f30e5aec01e263de73d6bfd2808d07b83f3
teacherbot <norrisa@faytechcc.edu>
2024-09-16 17:00:20 +0000

    m2t2 - form submission works

--- m2t1/hello.py
+++ m2t1/hello.py
@@ -1,17 +1,21 @@
 # minimal Flask app
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 #print("Starting flask program from ", __name__)
 app = Flask(__name__)
 # Do any app specific setup here
 # for instance, loading a database
 app.config["DEBUG"] = True # will run in debug mode
+# TODO: make this a database or something
+comments = []
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"]) # we allow posting
 def index():
-    # find the template in /templates
-    #name = "John"
-    return render_template("main_page.html")
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
+    # otherwise, it's a post
+    comments.append(request.form["contents"])
+    return redirect(url_for('index'))
 
 
 
